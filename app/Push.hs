c385bf593ee26b07520ed2ca59831983db5bc74a
mitsuji <mitsuji@na-s.jp>
2016-04-30 17:27:01 +0900

    ok

--- app/Push.hs
+++ app/Push.hs
@@ -1,34 +1,46 @@
 import Network.Transport.InMemory (createTransport)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
-import Control.Monad (forever)
 import Text.Read (readMaybe)
 
-processString :: (ProcessId, String) -> Process ()
-processString (sender,msg) = do
-  liftIO $ putStrLn $ "string: " ++ msg
-  send sender $ "*" ++ msg ++ "*"
+type State = Int
 
-processInt :: (ProcessId, Int) -> Process ()
-processInt (sender,n) = do
-  liftIO $ putStrLn $ "int: " ++ show n
+processString :: State -> (ProcessId, String) -> Process State
+processString count (sender,msg) = do
+  send sender $ "*" ++ msg ++ "*"
+  liftIO $ do
+    putStrLn $ "string: " ++ msg
+    putStrLn $ "count: " ++ show count
+  return $ count+1
+
+processInt :: State -> (ProcessId, Int) -> Process State
+processInt count (sender,n) = do
   send sender $ n * 2 + 1
+  liftIO $ do
+    putStrLn $ "int: " ++ show n
+    putStrLn $ "count: " ++ show count
+  return $ count+1
 
 main :: IO ()
 main = do
   node <- createTransport >>= (\t -> newLocalNode t initRemoteTable)
-  spid <- forkProcess node $ forever $
-          receiveWait [match processString, match processInt]
-  loop node spid
+  spid <- forkProcess node $ serverLoop 0
+          
+  clientLoop node spid
   where
-    loop node tpid = do
+    serverLoop state = do
+      state' <- receiveWait [match (processString state), match (processInt state)]
+      serverLoop state'
+      
+    clientLoop node tpid = do
       xs <- getLine
       runProcess node $ do
         self <- getSelfPid
         case readMaybe xs :: Maybe Int of
           Nothing -> sendString tpid self xs
           Just num -> sendInt tpid self num
-      loop node tpid
+      clientLoop node tpid
+      
     
 sendString tpid self xs = do
     send tpid (self,xs)
@@ -36,7 +48,8 @@ sendString tpid self xs = do
     case msb of
       Just sb -> liftIO $ putStrLn $ "result: " ++ sb
       Nothing -> return ()
-  
+
+
 sendInt tpid self num = do
     send tpid (self,num)
     msb <- expectTimeout 1000000 :: Process (Maybe Int)
